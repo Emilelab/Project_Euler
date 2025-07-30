d0d354095c35644e1dd1c239bcec91d5d734cff4
Emile <emile@localhost.localdomain>
2025-07-29 22:04:34 -0400

    Initial commit with existing files

---
+++ problem_4.py
@@ -0,0 +1,20 @@
+#Find the largest palindrome made from the product of two 3-digit numbers.
+
+def is_it_a_palindrome(n):
+    print("result is ", number)
+    s = str(n)
+    for z in range (3):
+        if s[0] == s[5]:
+            
+
+y = 9
+for x in range (9, 0, -1):
+    print("x is egal to ", x)
+    print("y is egal to ", y)    
+    result = x*y
+    is_it_a_palindrome(result)
+    for y in range (9, 0, -1):
+        print("x is egal to ", x)
+        print("y is egal to ", y)         
+        result = x*y
+        is_it_a_palindrome(result)
---
+++ test.py
@@ -0,0 +1,5 @@
+string = "kayak"
+if string[0]==string[4]:
+    print("true")
+else:
+    print("false")
