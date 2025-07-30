2baabf384c2d23c665ee2a0dcee5979afe5c2a61
Emile <emile@localhost.localdomain>
2025-07-30 12:37:19 -0400

    algorithm to solve probleme 4 of project euler, might revisit later for more elegant solution

--- problem_4.py
+++ problem_4.py
@@ -1,20 +1,59 @@
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
-def is_it_a_palindrome(n):
-    print("result is ", number)
-    s = str(n)
-    for z in range (3):
-        if s[0] == s[5]:
-            
+a = 0 #biggest palindrome
 
-y = 9
-for x in range (9, 0, -1):
-    print("x is egal to ", x)
-    print("y is egal to ", y)    
+def is_it_a_palindrome(n):
+    s = str(n)
+    l = len(s)
+    global a
+    if l == 6:
+        if s[0] == s[5]:
+            if s[1] == s[4]:
+                if s[2] == s[3]:
+                    if n > a:
+                        a = n
+                    else: return
+                else: return
+            else: return
+        else: return
+    elif l == 5:
+        if s[0] == s[4]:
+            if s[1] == s[3]:
+                if n > a:
+                    a = n
+                else: return
+            else: return
+        else: return
+    elif l == 4:
+        if s[0] == s[3]:
+            if s[1] == s[2]:
+                    if n > a:
+                        a = n
+                    else: return
+            else: return
+        else: return
+    elif l == 3:
+        if s[0] == s[2]:
+            if n > a:
+                a = n
+            else: return
+        else: return
+    elif l == 2:
+        if s[0] == s[1]:
+            if n > a:
+                a = n
+            else: return
+        else: return
+    elif l == 1:
+        if n > a:
+            a = n
+        else: return
+
+y = 999
+for x in range (999, 0, -1):  
     result = x*y
     is_it_a_palindrome(result)
-    for y in range (9, 0, -1):
-        print("x is egal to ", x)
-        print("y is egal to ", y)         
+    for y in range (999, 0, -1):       
         result = x*y
         is_it_a_palindrome(result)
+print("The biggest palindrome is", a)
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 string = "kayak"
+l = len(string)
+print(l)
 if string[0]==string[4]:
     print("true")
 else:
