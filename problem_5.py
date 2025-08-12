cb8bb44e6ffa5966695b6bcd2855d3376b4dd5d8
emile <20891391+Emilelab@users.noreply.github.com>
2025-08-12 10:12:36 -0400

    Tried to optimise the bruteforce method, still think that there is a better way

--- problem_5.py
+++ problem_5.py
@@ -3,17 +3,32 @@
 #What is the smallest positive number that is evenly divisible by all of the numbers from 
 #1 to 20?
 
-"""Brute Force, very long, very stupid
-for i in range (1, 10000000000000):
-    for j in range (1, 21):
+
+for i in range (20, 10000000000000, 20):
+    for j in range (20, 10, -1):
         print("Testing:",i)
         a = i%j
-        if a == 0 and j == 20:
+        if a == 0 and j == 11:
             print("The answer is:",i)
             exit()
         elif a == 0:
             continue
         else:
-            break"""
+            break
+"""
+Could I still 'brute force' but quicker and smarter?
+Don't need to test 1 because every positive int should be divisable by 1
+Don't need to test 10 if divisable be 20, etc.
+20
+19
+18
+17
+16
+15
+14
+13
+12
+11
 
-
+nope still dumb... got to 52533567 before shutting it down
+"""
